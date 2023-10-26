7a65d37e31813d550234f051a7ba64bcff46779e
Evvaog <evvaog@gmail.com>
2023-10-26 12:20:25 +0200

    change the name of the main page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<MainLayout />}>
               {" "}
-              <Route path="/kanban-board/" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/:task" element={<Task />} />
               <Route path="*" element={<NotFound />} />
             </Route>
