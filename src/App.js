1f200bba2edd9f715d133baec9402c0723d1c705
Evvaog <evvaog@gmail.com>
2023-10-26 11:54:51 +0200

    index to path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<MainLayout />}>
               {" "}
-              <Route index element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/:task" element={<Task />} />
               <Route path="*" element={<NotFound />} />
             </Route>
